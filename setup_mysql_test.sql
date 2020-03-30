1fae3569eb453b899334f3cab2c7e377c42e414b
Cristian <1154@holbertonschool.com>
2020-03-30 17:35:03 +0000

    prepares a MySQL server hbnb_test_db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- script that creates the MySQL server hbnb_test_db.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'; 
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
