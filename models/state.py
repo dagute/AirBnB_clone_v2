e7e3991b4d1a1a7704920c74562c841b546fd3d4
dagute <1271@holbertonschool.com>
2020-03-31 16:15:07 +0000

    add state and city

--- models/state.py
+++ models/state.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Getter"""
+        my_cities = [value for key, value in models.storage.all.items()
+                     if 'City' in key and value.state_id = self.id]
+        return my_cities
