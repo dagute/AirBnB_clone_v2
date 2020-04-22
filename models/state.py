235766b18e0f605c03fc3cb66e8d3edf4f332f98
dagute <1271@holbertonschool.com>
2020-04-22 17:22:16 +0000

    Improve engines

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, String
-from os import getenv
+import os
 import models
 
 
@@ -14,20 +14,15 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state", cascade="all, delete")
 
     else:
-        name = ""
 
-    @property
-    def cities(self):
-        """Getter"""
-        """ my_cities = [value for key, value in models.storage.all().items()
-                     if 'City' in key and value.state_id == self.id] """
-        all_cities = []
-        for city in models.storage.all("City").values():
-            if city.state_id == self.id:
-                all_cities.append(city)
-        return all_cities
+        @property
+        def cities(self):
+            """Getter"""
+            my_cities = [value for key, value in models.storage.all().items()
+                         if 'City' in key and value.state_id == self.id]
+            return my_cities
